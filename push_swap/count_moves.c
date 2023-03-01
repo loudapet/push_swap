6d875949dcfeee5e3aa8a647a1a4f09af73b9daf
loudapet <peta.louda95@gmail.com>
2023-03-01 16:30:24 +0100

    Count rr and rrr preparation

--- push_swap/count_moves.c
+++ push_swap/count_moves.c
@@ -6,11 +6,17 @@
 /*   By: plouda <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/01 14:13:22 by plouda            #+#    #+#             */
-/*   Updated: 2023/03/01 15:50:56 by plouda           ###   ########.fr       */
+/*   Updated: 2023/03/01 16:29:52 by plouda           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
+
+// v jakych pripadech pouzit otaceni zaroven (rr, rrr)
+int	count_if_double(int	rota_a, int rota_b, int rev_rota_a, int rev_rota_b)
+{
+	/*code*/
+}
 
 int	count_rota(t_clist *stack, int value)
 {
